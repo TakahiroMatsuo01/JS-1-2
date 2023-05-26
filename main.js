2f96166a79ad85bf90d9eeea6b8fb952b7a4e024
codewriterX <miturugi.infinite@gmail.com>
2023-05-26 22:37:07 +0900

    修正後３

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 while(1){
     let question = prompt("日本の首都は？");
-    if(question === '東京'){
-        alert('正解です！');
-    }else{
-        alert("不正解です！");
-    }
+        if(question === '東京'){
+            alert('正解です！');
+        }else{
+            alert("不正解です！");
+        }
 }
